a47fc9464bb0301e47e6eeb881dead84736f249a
Simple_Seeker <138399683+ZdobBurda@users.noreply.github.com>
2023-07-02 21:37:55 +0200

    Create Generator.py
    I think it should work

---
+++ Generator.py
@@ -0,0 +1,12 @@
+import random
+
+def generate_random_number(start, end):
+    return random.randint(start, end)
+
+# Example usage
+print(generate_random_number(1, 10))  # Output: Random number between 1 and 10 (inclusive)
+
+
+
+
+
